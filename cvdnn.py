c6796e178cf5bc50506dec9ba2c3902d0268bf39
mobangjack <mobangjack@foxmail.com>
2017-08-28 13:30:52 +0800

    test

--- cvdnn.py
+++ cvdnn.py
@@ -4,36 +4,56 @@ import time
 import cv2
 
 arp = ap.ArgumentParser()
-arp.add_argument("-i", "--image", required=True,
+arp.add_argument("-i", "--image", required=False,
 	help="path to input image")
 arp.add_argument("-p", "--prototxt", required=True,
 	help="path to Caffe 'deploy' prototxt file")
-arp.add_argument("-m", "--modle", required=True,
+arp.add_argument("-m", "--model", required=True,
 	help="path to Caffe pretrained model")
 arp.add_argument("-l", "--labels", required=True,
 	help="path to ImageNet labels (i.e., syn-sets)")
 args = vars(arp.parse_args())
 
-image = cv2.imread(args["image"])
+cap = cv2.VideoCapture()
+if (args["image"] is None) :
+   cap.open(0)
+else :
+   cap.open(args["image"])
+
+if (cap.isOpened() == False) :
+   print("ERROR: Fail to capture")
+   exit(0)
 
 rows = open(args["labels"]).read().strip().split("\n")
 classes = [r[r.find(" ") + 1:].split(",")[0] for r in rows]
 
-blob = cv2.dnn.blobFromImage(image, 1, (224, 224), (104, 117, 123))
-
 print("[INFO] loading model...")
-
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
-net.setInput(blob)
-start = time.time()
-preds = net.forward()
-end = time.time()
+   
+while True:
+   ret, image = cap.read()
+   if (image is None) : break
+   cv2.imshow("Image", image)
+   cv2.waitKey(1)
+   '''
+   blob = cv2.dnn.blobFromImage(image, 1, (224, 224), (104, 117, 123))
+   
+   net.setInput(blob)
+   start = time.time()
+   preds = net.forward()
+   end = time.time()
 
-print("[INFO] classification took {:.5} seconds".format(end - start))
+   print("[INFO] classification took {:.5} seconds".format(end - start))
 
-idxs = np.atgsort(preds[0])[::-1][:5]
-for (i, idx) in enumerate(idxs):
-	if i == 0:
-		text = "Label: {}, {:.2f}%".format(i + 1, classes[idx], preds[0][idx])
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+   idxs = np.argsort(preds[0])[::-1][:3]
+   for (i, idx) in enumerate(idxs):
+	   if i == 0:
+		   text = "Label: {}, {:.2f}%".format(classes[idx], preds[0][idx])
+		   cv2.putText(image, text, (5, 25),  cv2.FONT_HERSHEY_SIMPLEX,
+			   0.7, (0, 0, 255), 2)
+	   print("[INFO] {}. label: {}, probability: {:.5}".format(i + 1,
+		   classes[idx], preds[0][idx]))
+
+   cv2.imshow("Image", image)
+   cv2.waitKey(1)
+   '''
